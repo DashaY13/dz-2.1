025c6257f65e00c28dfa7b1ff882cdf50f16b0d3
ДашаY13 <dasha130289@rambler.ru>
2021-10-11 15:33:34 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20000;
+        int percentTicketPrice = 20;
+        int bonus = ticketPrice / percentTicketPrice;
+        System.out.println(bonus + "miles");
+    }
+    }
